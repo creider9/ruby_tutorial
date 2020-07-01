ef5889fbca17e7e203392cb0b246ce326cc892ea
EC2 Default User <ec2-user@ip-172-31-53-232.ec2.internal>
2020-07-01 00:51:42 +0000

    Initialize repository

---
+++ lib/creid_palidrome.rb
@@ -0,0 +1,6 @@
+require "creid_palidrome/version"
+
+module CreidPalidrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/creid_palidrome/version.rb
@@ -0,0 +1,3 @@
+module CreidPalidrome
+  VERSION = "0.1.0"
+end
